85a66ac291d42bc7493510fe213c6cc4ddf61246
WEIZIBIN <1999597@qq.com>
2016-06-26 01:01:33 +0800

    TickTick
    Take me a lot of time.

---
+++ hdu1007.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int count;
+	double x[100000];
+	double y[100000];
+	int i;
+	while(scanf("%d", &count))
+	{
+		if(count == 0)
+			break;
+
+
+
+	}
+	return 0;
+}
