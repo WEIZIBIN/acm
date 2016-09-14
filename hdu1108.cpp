095231f065aa1e84b18ff940a33b8c95d8d57e72
WEIZIBIN <1999597@qq.com>
2016-09-15 00:25:23 +0800

    1108

---
+++ hdu1108.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+	if(b==0) return a;
+	else return gcd(b,a%b);
+}
+
+int lcm(int a, int b){
+	return a*b/gcd(a,b);
+}
+
+int main(){
+	int a,b;
+	while(cin>>a>>b){
+		cout<<lcm(a,b)<<endl;
+	}
+}
